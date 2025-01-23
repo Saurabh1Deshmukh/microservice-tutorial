b31cc273c9090faacb8b9dc669d4ed190d50f97f
saurabh deshmukh <saurabh1deshmukh@gmail.com>
2025-01-23 21:17:54 +0530

    initial code commit
    - added services

---
+++ process-sftp-app-service/src/main/java/com/spring/app/demo/process_sftp/ProcessSftpApplication.java
@@ -0,0 +1,13 @@
+package com.spring.app.demo.process_sftp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessSftpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessSftpApplication.class, args);
+	}
+
+}
---
+++ process-sftp-app-service/src/test/java/com/spring/app/demo/process_sftp/ProcessSftpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.app.demo.process_sftp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessSftpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
