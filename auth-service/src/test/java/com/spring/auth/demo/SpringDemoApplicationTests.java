3e825f9cf1348c435c7187b48b18e1836a67d665
saurabh deshmukh <saurabh1deshmukh@gmail.com>
2025-01-23 21:56:47 +0530

    auth-service

---
+++ auth-service/src/test/java/com/spring/auth/demo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package src.test.java.com.spring.auth.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
